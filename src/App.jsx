68744c17a5688e240625ac887022f7489c90e2ac
aladdinalizada <tu7c5zneb@code.edu.az>
2024-02-22 01:54:08 +0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+// Import Layouts
+import Header from "./layouts/Header/Header";
+import Footer from "./layouts/Footer/Footer";
+// Import Pages
+import Hero from "./pages/Hero/Hero";
+import Partners from "./pages/Partners/Partners";
+import HelloThere from "./pages/Hello-there/HelloThere";
+import Services from "./pages/Services/Services";
+import Projects from "./pages/Projects/Projects";
+import Testimonial from "./pages/Testimonial/Testimonial";
+import ContactUs from "./pages/Contact-us/ContactUs";
+import OurBlog from "./pages/Our-Blog/OurBlog";
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Hero />
+        <Partners />
+        <HelloThere />
+        <Services />
+        <Projects />
+        <Testimonial />
+        <ContactUs />
+        <OurBlog />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Hello-there/HelloThere.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloThere = () => {
+  return <div>HelloThere</div>;
+};
+
+export default HelloThere;
