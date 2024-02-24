7951cef646c9f7e9e9e6a5eec8239d1bd504f908
aladdinalizada <tu7c5zneb@code.edu.az>
2024-02-24 20:52:12 +0400

    complete: partners components

--- src/pages/Partners/Partners.jsx
+++ src/pages/Partners/Partners.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
+import "./partners.css";
+import Casa from "/assets/pages/partners/casa.png";
+import Fimela from "/assets/pages/partners/fimela.png";
+import Pnn from "/assets/pages/partners/pnn.png";
 const Partners = () => {
-  return <div>Partners</div>;
+  return (
+    <div className="partnersContainer">
+      <div className="partnersTitle">
+        <h2>Trusted by 75+ Partners</h2>
+      </div>
+      <div className="partnersLogo">
+        <div className="partnersImg">
+          <img src={Casa} alt="Casa" />
+        </div>
+        <div className="partnersImg">
+          <img src={Fimela} alt="Fimela" />
+        </div>
+        <div className="partnersImg">
+          <img src={Pnn} alt="Pnn" />
+        </div>
+        <div className="partnersImg">
+          <img src={Casa} alt="Casa" />
+        </div>
+        <div className="partnersImg">
+          <img src={Fimela} alt="Fimela" />
+        </div>
+        <div className="partnersImg">
+          <img src={Pnn} alt="Pnn" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
